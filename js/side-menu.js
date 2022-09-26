8edb7ae74c9c25aebf8b82c8242efb3fe1039893
Luxana1 <96484101+Luxana1@users.noreply.github.com>
2022-09-26 11:51:34 +0100

    hamburger menu finction
    I wrote a simple function that toggles a class "is-active" to my hamburger (button) and makes it transform from bars to the "X" one. I tested the function and it works.

--- js/side-menu.js
+++ js/side-menu.js
@@ -1,20 +1,5 @@
-function openNav() {
-    hamburger.classList.add("is-active");
-    document.getElementById("popUpNav").style.display = "block";
-}
+const hamburger = document.querySelector(".hamburger");
 
-function closeNav() {
-    hamburger.classList.remove("is-active");
-    document.getElementById("popUpNav").style.display = "none";
-}
-
-var hamburger = document.querySelector(".hamburger");
-hamburger.addEventListener("click", function() {
-    hamburger.addEventListener("click", function() {
-        if (hamburger.classList.contains("is-active")) {
-            closeNav();
-        } else {
-            openNav();
-        }
-    })
-});
+hamburger.addEventListener("click", function () {
+    hamburger.classList.toggle("is-active");
+    });
