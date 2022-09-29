4a9f944d5940440f2e7d5e4af02e558f46cb4668
Luxana1 <96484101+Luxana1@users.noreply.github.com>
2022-09-29 13:10:44 +0100

    minor changes
    I removed some code, that I didn't need in my carousel.js. I created a new js file for my sticky header function.

--- js/carousel.js
+++ js/carousel.js
@@ -4,8 +4,6 @@ $(document).ready(function () {
         arrows: false,
         infinite: true,
         speed: 300,
-        slidesToShow: 1,
-        adaptiveHeight: true,
         autoplay: true,
         autoplaySpeed: 5000
     });
