2c85736443ba6f801fc5ad149de209c5f9bd249a
Luxana1 <96484101+Luxana1@users.noreply.github.com>
2022-10-03 09:51:50 +0100

    Side menu
    I made some changes to my index.html. I moved the side menu elements to a div above my header and added the onClick() element to it. I wrote a JavaScript function that adds or removes classes from side menu divs and whole body elements and moves the website's content to the left when the button is clicked and the menu opens. The only issue there is that one class that I want to add to the body element (that moves the content) is undefined so it needs to be checked and resolved.

--- js/side-menu.js
+++ js/side-menu.js
@@ -1,5 +1,28 @@
+//button function for side menu --spin
 const hamburger = document.querySelector(".hamburger");
 
 hamburger.addEventListener("click", function () {
     hamburger.classList.toggle("is-active");
     });
+
+//function to open and close the side menu
+const sideMenu = document.getElementById("sideMenu");
+const mainContent = document.getElementById("mainContent");
+const mainContentOverlay = document.getElementById("mainContentOverlay");
+const body = document.getElementsByClassName("wrapper");
+
+function openNav() {
+    sideMenu.classList.add("is-active");
+    mainContent.classList.add("inactive");
+    body.style.overflow = "hidden";
+    mainContentOverlay.style.width = "100%";
+    mainContentOverlay.style.opacity = "1";
+}
+
+function closeNav() {
+    sideMenu.classList.remove("is-active");
+    mainContent.classList.remove("inactive");
+    body.style.overflow = "auto";
+    mainContentOverlay.style.width = "0%";
+    mainContentOverlay.style.opacity = "0";
+}
